833743930b5228fbf2f34ed21d42860f15d1fbf6
anthony-zhdanov <anton@vzc.ca>
2026-02-26 16:58:12 -0500

    Implemented simplest Tensor class methods

--- autograd_engine/tensor.py
+++ autograd_engine/tensor.py
@@ -15,3 +15,23 @@ class Tensor:
     
     def __repr__(self):
         return f"Tensor(data={self.data}, grad={self.grad})"
+
+    def __add__(self, other): 
+        result = Tensor(self.data + other.data, _children=(self, other), _op='+')
+        return result
+    
+    def __mul__(self, other):
+        result = Tensor(self.data * other.data, _children=(self, other), _op='*')
+        return result
+
+if __name__ == "__main__":
+    a = Tensor([2.0])
+    b = Tensor([3.0])
+    c = a * b
+    d = c + a
+
+    print(a)
+    print(c)
+    print(d)
+    print(d._prev)
+    print(d._op)
